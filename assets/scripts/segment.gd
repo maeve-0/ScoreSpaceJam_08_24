e30d67cba1c9c3a5e00e371895955054a9d7b8f9
Adam Sadlik <72628208+abam-iksde@users.noreply.github.com>
2024-08-31 20:49:10 +0200

    Implement lava timer

--- assets/scripts/segment.gd
+++ assets/scripts/segment.gd
@@ -5,7 +5,7 @@ var appearance_speed = 60.0
 var material: ShaderMaterial
 var player: Player
 
-@export var life_time := 4.0
+@export var life_time := 30.0
 
 var grow_amount := 0.0
 
@@ -22,11 +22,15 @@ func _ready():
 	(get_node('mesh_instance') as MeshInstance3D).set_surface_override_material(0, material)
 	grow_amount = -30.0
 	material.set_shader_parameter('albedo', colors[randi() % len(colors)])
+	material.set_shader_parameter('grow', grow_amount)
 
 
 func _physics_process(delta: float) -> void:
 	life_time -= delta
 	material.set_shader_parameter('grow', grow_amount)
+	material.set_shader_parameter('lava_state', LavaControl.state)
+	material.set_shader_parameter('lava_next_state', LavaControl.next_state)
+	material.set_shader_parameter('lava_next_progress', LavaControl.next_state_progress)
 	if player:
 		material.set_shader_parameter('player_z', player.global_position.z - global_position.z)
 	if life_time <= 0.0:
