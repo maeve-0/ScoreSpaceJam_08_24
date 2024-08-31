c11cfc16e630987119aa2b0227669b2893232266
Adam Sadlik <72628208+abam-iksde@users.noreply.github.com>
2024-08-31 21:16:46 +0200

    Disable grabbables on lava

--- assets/scripts/grabable.gd
+++ assets/scripts/grabable.gd
@@ -2,6 +2,9 @@ class_name Grabbable
 extends Area3D
 
 
+const _45_DEGREES = deg_to_rad(45.0)
+
+
 @onready var collision_shape := get_node('collision_shape') as CollisionShape3D
 @onready var particle_emitter := get_node('particle_emitter') as CPUParticles3D
 
@@ -17,3 +20,21 @@ func generate():
 	(collision_shape.shape as BoxShape3D).size = Vector3(size.x, 0.2, size.y)
 	particle_emitter.emission_box_extents = Vector3(size.x, 0.0, size.y) / 2.0
 	particle_emitter.amount = size.x * size.y
+
+
+func _physics_process(delta: float) -> void:
+	if LavaControl.state == LavaControl.State.NONE:
+		collision_shape.disabled = false
+		particle_emitter.emitting = true
+		return
+
+	var lava_normal: Vector3 = {
+		LavaControl.State.FLOOR: Vector3.UP,
+		LavaControl.State.LEFT_WALL: Vector3.RIGHT,
+		LavaControl.State.RIGHT_WALL: Vector3.LEFT,
+		LavaControl.State.CEILING: Vector3.DOWN,
+	}[LavaControl.state]
+
+	var disabled := lava_normal.angle_to(global_transform.basis.y) < _45_DEGREES
+	collision_shape.disabled = disabled
+	particle_emitter.emitting = not disabled
