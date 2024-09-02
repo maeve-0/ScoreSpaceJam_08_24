b1d65f0ab2f2320eab05cf9c45ce76758c268f96
Adam Sadlik <72628208+abam-iksde@users.noreply.github.com>
2024-09-02 10:08:23 +0200

    Add coyote time and lock mouse on restart

--- assets/scripts/death_screen.gd
+++ assets/scripts/death_screen.gd
@@ -16,7 +16,19 @@ func _ready() -> void:
 
 	get_node('restart_button').connect('pressed', func():
 		get_tree().reload_current_scene()
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		Globals.player_health = 1.0
 	)
+
+
+func _input(event: InputEvent):
+	if not visible:
+		return
+	if event is InputEventKey:
+		if event.keycode == KEY_R:
+			get_tree().reload_current_scene()
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+			Globals.player_health = 1.0
 
 
 func _physics_process(delta: float) -> void:
@@ -26,5 +38,3 @@ func _physics_process(delta: float) -> void:
 		if not was_visible:
 			Scoreboard._upload_score(Globals.score)
 			was_visible = true
-		if Input.is_key_pressed(KEY_R):
-			get_tree().reload_current_scene()
--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -17,6 +17,8 @@ const MAX_HORIZONTAL_VELOCITY = 12.0
 const HORIZONTAL_DAMP = 20.0
 const HORIZONTAL_ACCELERATION = 10.0
 const _90_DEGREES = deg_to_rad(90.0)
+const COYOTE_TIME = 0.2
+var coyote_time := 0.0
 
 
 var hand: Hand
@@ -49,7 +51,10 @@ func _physics_process(delta: float) -> void:
 	var player_input := collect_input()
 
 	up_vector.look_at(global_position + Vector3(0.0, 0.0, -1.0), up_direction)
-	if not is_on_floor():
+	if is_on_floor():
+		coyote_time = COYOTE_TIME
+	else:
+		coyote_time -= delta
 		velocity += up_direction * get_gravity().y * delta
 
 	if player_input['grapple'] and not hand:
@@ -61,7 +66,7 @@ func _physics_process(delta: float) -> void:
 		grapple.global_rotation = camera.global_rotation
 		hand = grapple
 
-	if player_input['jump'] and is_on_floor():
+	if player_input['jump'] and coyote_time > 0.0:
 		velocity = up_direction * JUMP_VELOCITY
 
 	if player_input['attack'] and Globals.player_attack_time <= 0:
